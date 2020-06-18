3828491c1e78cf46304ce00d149720d350f9fda2
B CASTILLO <bryancastillo44@outlook.com>
2020-06-18 19:34:44 -0400

    Added Click

--- notes.py
+++ notes.py
@@ -1,9 +1,37 @@
 from peewee import *
+import click
 
 db = PostgresqlDatabase('notetaker', user='postgres', password='',
                         host='localhost', port=5432)
 
 db.connect()
+
+
+@click.group()
+@click.version_option(version='1.0.0', prog_name='NoteTaker')
+def main():
+    """Hi! I'm Note Taker, I'm here to help you take Notes!""" 
+
+    pass
+
+# Create notes
+
+@main.command()
+@click.option('--title','-ti',prompt=True)
+
+
+def add_note(title):
+    """ Add a New Note """
+
+    print(title)
+
+
+# Read notes
+# Select specific note
+
+if __name__ == "__main__":
+    main()
+
 
 class BaseModel(Model):
     class Meta:
@@ -13,8 +41,10 @@ class Note(BaseModel):
     title = CharField()
     contents = TextField()
 
+
 db.drop_tables([Note])
 db.create_tables([Note])
+
 
 note1 = Note(title='Note1', contents='This is the first note')
 note1.save()
